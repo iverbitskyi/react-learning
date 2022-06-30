ab95bcdf0a9f402c5f8d28579c7252c1bfe9083e
v_verbitskyi <ivan.verbitskyj@gmail.com>
2022-06-30 09:34:07 +0300

    finished landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import NewsComponent from './components/NewsComponent';
+import React from "react";
+import "./App.css";
+import travel_01 from "./assets/travel-01.jpg";
+import travel_02 from "./assets/travel-02.jpg";
+import travel_03 from "./assets/travel-03.jpg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Trips" },
+    { url: "#", title: "Rewards" },
+  ];
+
   return (
     <div className="App">
-      <NewsComponent />
+      <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={travel_01} />
+      <Slider
+        imageSrc={travel_02}
+        title={"Be an explorer."}
+        subtitle={
+          "Our platform offers a wide variety of unique travel locations!"
+        }
+      />
+      <Slider
+        imageSrc={travel_03}
+        title={"Memories for a lifetime."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        flipped={true}
+      />
     </div>
   );
 }
